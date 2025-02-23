19488fdff37e45c559b2d80c7a5ca56a9533e661
Amanda Figueroa <38810464+Cleanvalidation@users.noreply.github.com>
2025-02-23 18:55:03 -0500

    finished TPPbenchmark function that uses Msstats processed data

--- R/plot_benchmarksTPP.R
+++ R/plot_benchmarksTPP.R
@@ -24,7 +24,7 @@ plot_benchmarksTPP<-function(result,temps=unique(result$temperature),shifter="st
     dplyr::filter(Protein %in% unique(result$Protein)[2])|>
     dplyr::mutate(Accession=Protein)|>
     dplyr::ungroup()
-  png(filename = paste0("ProfilePlots_",shifter,design,"_MsstatsTMTproc.png"),
+  png(filename = paste0("ProfilePlots_",shifter,design,"_TPPproc.png"),
       width =1600, height = 1600, units = "px", pointsize = 12,
       res = 130,type ="cairo")
   Profile_plot<-ggplot(One_prot_ICC,mapping=aes(x=Condition,y=Abundance,color=treatment))+geom_point()+
@@ -39,60 +39,54 @@ plot_benchmarksTPP<-function(result,temps=unique(result$temperature),shifter="st
  dataTPP$ProteinLevelData$Condition<-stringr::str_extract(dataTPP$ProteinLevelData$treatment,"[[:lower:]]+")
  #set technical replicate
  dataTPP$ProteinLevelData$TechRepMixture<-stringr::str_extract(dataTPP$ProteinLevelData$Subject,"_[:digit:]")
+ #reference channel
+ refChannel<-dataTPP$ProteinLevelData$Channel[which(unique(dataTPP$ProteinLevelData$temperature)==min(unique(dataTPP$ProteinLevelData$temperature)))]
+ #add necessary column names
+ dataTPP$ProteinLevelData$Accession<-dataTPP$ProteinLevelData$Protein
+ dataTPP$ProteinLevelData$File.ID<-dataTPP$ProteinLevelData$Subject
+ #normalize with TPP dataTPP<-TPP_normalization(dataTPP$ProteinLevelData,TPPfilters=FALSE,temps=unique(temps),reference=refChannel,CARRIER=FALSE)
  #Run data with TPP splines
-  TPP<-TPP_NPARC_calc(dataTPP$ProteinLevelData,method="NPARC",DF=5,CARRIER=FALSE,temps=set_temps(10,c(37.3, 40.6, 43.9, 47.2, 50.5, 53.8, 57.1, 60.4, 64, 67)),NORM=FALSE,filters=TRUE)
+  TPP<-TPP_NPARC_calc(dataTPP$normData,method="NPARC",DF=5,CARRIER=FALSE,temps=set_temps(10,c(37.3, 40.6, 43.9, 47.2, 50.5, 53.8, 57.1, 60.4, 64, 67)),NORM=FALSE,filters=TRUE)
 
-
-    MSstatsTMT::groupComparisonTMT(
-    dataMSstat,
-    contrast.matrix = comparison,
-    moderated = FALSE,
-    adj.method = "BH",
-    remove_norm_channel = TRUE,
-    remove_empty_channel = TRUE,
-    save_fitted_models = TRUE,
-    use_log_file = FALSE,
-    append = FALSE,
-    verbose = TRUE,
-    log_file_path = NULL
-  )
-  ATE_MSstats$ComparisonResult$ICC<-stringr::str_extract(ATE_MSstats$ComparisonResult$Protein,"icc_[:digit:].[[:digit:]]+")
-  ATE_MSstats$ComparisonResult$ICC<-paste0("% of bio var = ",100*as.numeric(stringr::str_extract(ATE_MSstats$ComparisonResult$ICC,"[:digit:].[[:digit:]]+")))
-  ATE_MSstats$ComparisonResult<-ATE_MSstats$ComparisonResult[stringr::str_detect(ATE_MSstats$ComparisonResult$ICC,"5|40"),]
-  ATE_MSstats$ComparisonResult<-ATE_MSstats$ComparisonResult|>dplyr::group_by(ICC)|>dplyr::mutate(Sens=100*sum(pvalue<0.001)/length(unique(Protein)))
-  png(filename = paste0("Histogram_",shifter,design,"_MsstatsTMTproc.png"),
+  TPP$ICC<-stringr::str_extract(TPP$uniqueID,"icc_[:digit:].[[:digit:]]+")
+  #append biological variance text to numeric values
+  TPP$ICC<-paste0("% of bio var = ",100*as.numeric(stringr::str_extract(TPP$ICC,"[:digit:].[[:digit:]]+")))
+  #Keep 5 and 40% ICC
+  TPP<-TPP[stringr::str_detect(TPP$ICC,"5|40"),]
+  TPP<-TPP|>dplyr::group_by(ICC)|>dplyr::mutate(Sens=100*sum(p_adj_NPARC<0.001)/length(unique(TPP$uniqueID)))
+  png(filename = paste0("Histogram_",shifter,design,"_TPPproc.png"),
       width =600, height = 600, units = "px", pointsize = 12,
       res = 130,type ="cairo")
   if(design=="onePot"){
     if(shifter=="strong"){
-      MSstat_hist<-ggplot2::ggplot(ATE_MSstats$ComparisonResult,mapping=aes(x=pvalue))+
+      TPP_hist<-ggplot2::ggplot(TPP,mapping=aes(x=p_adj_NPARC))+
         geom_histogram(fill="#030366",color="black",bins=1,binwidth = 0.025)+facet_wrap(~factor(ICC,levels=c("% of bio var = 5","% of bio var = 40")),nrow=1)+
         coord_cartesian(xlim = c(0, 1))+ylim(0,1000)+xlab("pvalue")+
         theme(text=element_text(size=15),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+coord_cartesian(xlim = c(0, 1))+
         scale_x_continuous(breaks=c(0,0.2,0.4,0.6,0.8,1.0))+
         geom_text(mapping=aes(x=0.5,y=800),
-                  label=paste0(ATE_MSstats$ComparisonResult$Sens," %"),size=6)+ylab("protein count")
+                  label=paste0(TPP$Sens," %"),size=6)+ylab("protein count")
     }else{
-      MSstat_hist<-ggplot2::ggplot(ATE_MSstats$ComparisonResult,mapping=aes(x=pvalue))+
+      TPP_hist<-ggplot2::ggplot(TPP$ComparisonResult,mapping=aes(x=p_adj_NPARC))+
         geom_histogram(fill="#030366",color="black")+facet_wrap(~factor(ICC,levels=c("% of bio var = 5","% of bio var = 40")),nrow=1)+
         scale_x_continuous(n.breaks=8)+ylim(0,1000)+xlab("pvalue")+
         theme(text=element_text(size=15),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+coord_cartesian(xlim = c(0, 1))+
         scale_x_continuous(breaks=c(0,0.2,0.4,0.6,0.8,1.0))+
         geom_text(mapping=aes(x=0.5,y=800),
-                  label=paste0(ATE_MSstats$ComparisonResult$Sens," %"),size=6)+ylab("protein count")
+                  label=paste0(TPP$Sens," %"),size=6)+ylab("protein count")
     }
   }else{#if this is a TPP design
-    MSstat_hist<-ggplot2::ggplot(ATE_MSstats$ComparisonResult,mapping=aes(x=pvalue))+
+    TPP_hist<-ggplot2::ggplot(TPP$ComparisonResult,mapping=aes(x=p_adj_NPARC))+
       geom_histogram(fill="#2C7FB8",color="black")+facet_wrap(~factor(ICC,levels=c("% of bio var = 5","% of bio var = 40")),nrow=1)+
       ylim(0,1000)+xlab("pvalue")+ scale_x_continuous(n.breaks=8)+
       theme(text=element_text(size=15),axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+coord_cartesian(xlim = c(0, 1))+
       scale_x_continuous(breaks=c(0,0.2,0.4,0.6,0.8,1.0))+
       geom_text(mapping=aes(x=0.5,y=800),
-                label=paste0(ATE_MSstats$ComparisonResult$Sens," %"),size=6)+ylab("protein count")
+                label=paste0(TPP$Sens," %"),size=6)+ylab("protein count")
 
   }
-  MSstat_hist
+  TPP_hist
   dev.off()
-  saveRDS(MSstat_hist,paste0("Histogram_",shifter,design,"_MsstatsTMTproc.RDS"))
+  saveRDS(TPP_hist,paste0("Histogram_",shifter,design,"_TPPproc.RDS"))
 
 }
