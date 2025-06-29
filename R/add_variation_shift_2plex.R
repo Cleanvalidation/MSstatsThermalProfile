0bab0051ff6babe7a900cbefd617498af22ac870
Amanda Figueroa <38810464+Cleanvalidation@users.noreply.github.com>
2025-06-29 18:24:58 -0400

    checking vignette 11 functions

--- R/add_variation_shift_2plex.R
+++ R/add_variation_shift_2plex.R
@@ -12,22 +12,30 @@ add_variation_shift_2plex <- function(template, icc, n_conditions, n_replicates,
 
   if (design == "TPP") {
     # Each temperature needs a channel. 10 temps * 2 subjects = 20 channels max.
-    if (n_temps %in% c(1, 2, 5, 10)) {
-      n_subjects <- 20 / (n_temps * n_conditions)
-      if (n_subjects != floor(n_subjects)) {
-        stop("Number of subjects is not an integer. Reduce n_temps or check design.")
-      }
-    } else {
-      stop("Please define n_temps as 1, 2, 5, or 10")
-    }
+    n_subjects<-switch(as.character(n_temps),
+                       "1" = {
+                         20
+                       },
+                       "2" = {
+                         10
+                       },
+                       "5" = {
+                         4
+                       },
+                       "10" = {
+                         2
+                       },
+                       {
+                         stop("Unsupported number of temperatures. Must be 1, 2, 5, or 10.")
+                       })
   } else if (design == "OnePot" && n_temps == 2) {
 
-    n_subjects <- 20 / (n_conditions * n_replicates)
+    n_subjects <- (n_conditions * n_replicates)
     n_obs <- n_subjects
 
   } else if (design == "OnePot" && n_temps == 5)  {
-    n_subjects <- 20 / (n_conditions * n_replicates)
-    n_obs <- n_subjects * n_conditions * n_replicates  # or just n_obs <- 20
+    n_subjects <- (n_conditions * n_replicates)
+    n_obs <- n_subjects
   }
 
 
@@ -77,51 +85,68 @@ add_variation_shift_2plex <- function(template, icc, n_conditions, n_replicates,
 
   } else if (n_temps == 2) {
     n_subjects <- n_replicates
-    modified_matrix <- matrix(NA, nrow = 20, ncol = n_conditions + 2)
+    modified_matrix <- matrix(NA, nrow = 20,ncol=n_conditions + 2)
     template_temp <- template %>% arrange(temperature)
-    temperatures <- unique(template_temp$temperature)[t_range]
+    temperatures <- sort(unique(template_temp$temperature))[t_range]
 
     for (i in 1:n_conditions) {
       for (j in 1:n_subjects) {
         sample_u <- rnorm(1, 0, sqrt(biovar))
+
         for (k in 1:n_temps) {
           current_responses <- template$Abundance[template$Condition == i][t_range[k]]
           sample_e <- rnorm(1, 0, sqrt(re_var))
           Y_true <- as.numeric(current_responses)
-          Y_observed <- Y_true + sample_u + sample_e
-          modified_matrix[(j - 1) * n_temps + k, i] <- Y_observed
-          modified_matrix[(j - 1) * n_temps + k, n_conditions + 1] <- temperatures[k]
-          modified_matrix[(j - 1) * n_temps + k, n_conditions + 2] <- j
+          Y_observed <- Y_true + sample_u + sample_e#add biological variance and error term
+          #for a separate function, unlog Y_observed, average Y_obs for all temps, log
+          modified_matrix[(j - 1)*n_temps + k , i] <- Y_observed
+          modified_matrix[(j - 1)*n_temps + k , n_conditions + 1] <- temperatures[k]#set temperature
+          modified_matrix[(j - 1)*n_temps + k , n_conditions + 2] <- j#set Subject
         }
       }
     }
 
-    colnames(modified_matrix) <- c(seq_len(n_conditions), "temperature", "Subject")
+    colnames(modified_matrix) <- c(seq(n_conditions), "temperature","Subject")
     modified_df <- as.data.frame(modified_matrix)
 
-    result <- modified_df %>%
-      pivot_longer(cols = -c(Subject, temperature), values_to = "Abundance", names_to = "Condition") %>%
-      group_by(Subject, Condition) %>%
-      mutate(Condition = ifelse(Condition == 1, "vehicle", "treated")) %>%
-      group_split()
+    result<-modified_df|>as.data.frame()|>tidyr::pivot_longer(cols=colnames(modified_df)[colnames(modified_df)!="Subject"&colnames(modified_df)!="temperature"],
+                                                              values_to="Abundance",
+                                                              names_to="Condition")|>
+      dplyr::group_by(Subject,Condition)|>
+      dplyr::mutate(Condition=ifelse(Condition==1,"vehicle","treated"))|>
+      dplyr::group_split()
+    if(design=="OnePot"){
+      YonePot_cr<-purrr::map2(result,seq(length(result)),function(x,y) {
+        X_obs_crt<-x|>dplyr::mutate(Abundance=2^Abundance)#unlog
+        XonePotcr<-X_obs_crt|>dplyr::mutate(Abundance=mean(Abundance,na.rm=T))#average
+        YonePot_cr<-XonePotcr|>dplyr::mutate(Abundance=log2(Abundance))#log back
+        #collapse temperature after pooling
+        YonePot_cr<-YonePot_cr|>
+          dplyr::select(-temperature)|>
+          dplyr::distinct()|>
+          dplyr::mutate(BioReplicate=y,
+                        Subject=y,
+                        Mixture=Condition)
+      })
+      result<-dplyr::bind_rows(YonePot_cr)
 
-    if (design == "OnePot") {
-      result <- purrr::map2(result, seq_along(result), function(x, y) {
-        x %>% mutate(Abundance = 2^Abundance) %>%
-          mutate(Abundance = log2(mean(Abundance, na.rm = TRUE))) %>%
-          select(-temperature) %>%
-          distinct() %>%
-          mutate(BioReplicate = y, Subject = y, Mixture = Condition)
-      }) %>% bind_rows()
-    } else if (design == "TPP") {
-      result <- bind_rows(result) %>%
-        mutate(BioReplicate = Subject, Mixture = Condition, Run = Condition, TechRepMixture = 1)
-      Channel_subject_mapping <- data.frame(
-        Subject = unique(result$Subject),
-        Channel = rep(set_temps(10, 1:10)$Channel, length.out = length(unique(result$Subject)))
-      )
-      result <- result %>% inner_join(Channel_subject_mapping, by = "Subject") %>%
-        mutate(Mixture = ifelse(Condition == 1, "vehicle", "treated"))
+
+    }else if(design=="TPP"){
+      result<-dplyr::bind_rows(result)
+      #generate temperature values selected
+      result<-result|>dplyr::mutate(BioReplicate=Subject,
+                                    Mixture=Condition,
+                                    Run=Condition)|>
+        dplyr::arrange(Condition)
+      result$TechRepMixture<-1
+      Channel_subject_mapping<-data.frame(Channel=rep(set_temps(10,c(1,2,3,4,5,6,7,8,9,10))$Channel,2),
+                                          Subject=unique(result$Subject))|>dplyr::distinct()
+
+      result<-result|>dplyr::inner_join(Channel_subject_mapping,relationship="many-to-many")|>
+
+        dplyr::mutate(Mixture=ifelse(Condition==1,"vehicle","treated"))
+
+      #return(result)
     }
 
     result <- bind_rows(result)
